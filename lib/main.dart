9af7f27c7e5988496eef3e7afff696791f22a378
Carlos <dominguezalacid@gmail.com>
2025-01-28 14:17:36 +0100

    actualizar

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:logging/logging.dart';
+
+Logger _log = Logger('MyApp');
 
 void main() {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((record) {
+    _log.info('${record.level.name}: ${record.time}: ${record.message}');
+  });
   runApp(MyApp());
 }
 
@@ -32,34 +38,55 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   final _textController = TextEditingController();
   final List<ChatMessage> _messages = [];
+  bool _isLoading = false;
 
   void _sendMessageToRasa() async {
-  final message = _textController.text.trim();
-  if (message.isNotEmpty) {
-    setState(() {
-      _messages.add(ChatMessage(
-        text: message,
-        isMe: true,
-      ));
-      _textController.clear();
-    });
-    final response = await http.post(
-      Uri.parse('http://localhost:5005/webhooks/rest/webhook'),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'message': message}),
-    );
-    if (response.statusCode == 200) {
-      final responseData = jsonDecode(response.body);
-      final botResponse = responseData['messages'][0]['content'];
+    _log.info('Enviando mensaje a Rasa...');
+    final message = _textController.text.trim();
+    if (message.isNotEmpty) {
       setState(() {
         _messages.add(ChatMessage(
-          text: botResponse,
-          isMe: false,
+          text: message,
+          isMe: true,
         ));
+        _textController.clear();
       });
+      _isLoading = true;
+      _log.info('Mensaje enviado: $message');
+      final response = await http.post(
+        Uri.parse('http://localhost:5005/conversations/default/respond'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'message': message}),
+      );
+      _log.info('Respuesta de Rasa: ${response.statusCode}');
+      _isLoading = false;
+      if (response.statusCode == 200) {
+        _log.info('Respuesta de Rasa: ${response.body}');
+        final responseData = jsonDecode(response.body);
+        final botResponse = responseData['messages'][0]['content'];
+        setState(() {
+          _messages.add(ChatMessage(
+            text: botResponse,
+            isMe: false,
+          ));
+        });
+      } else {
+        _log.info('Error al enviar mensaje: ${response.statusCode}');
+        if (mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Error al enviar mensaje'),
+            ),
+          );
+        }
+      }
     }
   }
-}
+
+  void _sendVoiceMessage() async {
+    // Implementar la lógica para enviar un mensaje de voz a Rasa
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -94,9 +121,18 @@ class _ChatScreenState extends State<ChatScreen> {
                   onPressed: _sendMessageToRasa,
                   child: Text('Enviar'),
                 ),
+                SizedBox(width: 16),
+                ElevatedButton(
+                  onPressed: _sendVoiceMessage,
+                  child: Icon(Icons.mic),
+                ),
               ],
             ),
           ),
+          if (_isLoading)
+            Center(
+              child: CircularProgressIndicator(),
+            ),
         ],
       ),
     );
